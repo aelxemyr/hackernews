02b84b4ea71b496ec702a1ecefd7f74730e5882c
Alex Myers <vulpinoctum@gmail.com>
2017-11-08 12:07:17 -0700

    Create dismiss button for list.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const list = [
@@ -21,26 +20,53 @@ const list = [
   },
 ];
 
-const listItem = item => (
-  <div>
-    <li>
-      <a href={item.url}>{item.title}</a>
-    </li>
-    <li>{item.author}</li>
-    <li>{item.num_comments}</li>
-    <li>{item.points}</li>
-  </div>
-)
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      header: 'This is the Title',
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
   render() {
+    const listItem = item => (
+      <div>
+        <li>
+          <a href={item.url}>{item.title}</a>
+        </li>
+        <li>{item.author}</li>
+        <li>{item.num_comments}</li>
+        <li>{item.points}</li>
+      </div>
+    );
+
     return (
       <div className="App">
-        {list.map(item =>
-          <ul key={item.objectID}>
-            {listItem(item)}
-          </ul>
-        )}
+        <h1 style={{ textAlign: 'center' }}>{this.state.header}</h1>
+        {this.state.list.map(item => (
+          <div>
+            <ul key={item.objectID}>
+              {listItem(item)}
+            </ul>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
       </div>
     );
   }

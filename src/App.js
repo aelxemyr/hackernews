e899efb1e8e9edf5a34ae7c53d3d685ba3323023
Alex Myers <vulpinoctum@gmail.com>
2017-11-08 15:02:33 -0700

    Add data fetching.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/vi';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      header: 'This is the Title',
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
   }
+
+  setSearchTopStories = result => this.setState({ result });
+
+  fetchSearchTopStories = searchTerm =>
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
 
   onDismiss = id => {
     const updatedList = this.state.list.filter(item => item.objectID !== id);
@@ -39,13 +33,19 @@ class App extends Component {
   onSearchChange = event =>
     this.setState({ searchTerm: event.target.value });
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+  }
+
   render() {
-    const { header, searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if(!result) { return null; }
 
     return (
       <div className="page">
         <div className="interactions">
-          <h1>{header}</h1>
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
@@ -54,7 +54,7 @@ class App extends Component {
           </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
